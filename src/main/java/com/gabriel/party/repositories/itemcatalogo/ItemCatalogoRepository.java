613088b63f00bed21d077231ddafb6fddb5b2a9a
Gabriel de oliveira <oliveirafrerreira97@hotmail.com>
2026-06-02 21:10:08 -0300

    feat: adicionado a busca por categoria no metodo 'buscarsemcategoria'

--- src/main/java/com/gabriel/party/repositories/itemcatalogo/ItemCatalogoRepository.java
+++ src/main/java/com/gabriel/party/repositories/itemcatalogo/ItemCatalogoRepository.java
@@ -30,7 +30,8 @@ public interface ItemCatalogoRepository extends JpaRepository<ItemCatalogo, UUID
             WHERE i.ativo = true
               AND p.ativo = true
               AND (:busca = '' OR LOWER(i.titulo) LIKE LOWER(CONCAT('%', :busca, '%'))
-                               OR LOWER(i.descricao) LIKE LOWER(CONCAT('%', :busca, '%')))
+                               OR LOWER(i.descricao) LIKE LOWER(CONCAT('%', :busca, '%'))
+                               OR LOWER(i.categoria.nome) LIKE LOWER(CONCAT('%', :busca, '%')))
               AND (:cidade = '' OR LOWER(p.endereco.cidade) LIKE LOWER(CONCAT('%', :cidade, '%')))
             """,
         countQuery = """
@@ -38,7 +39,8 @@ public interface ItemCatalogoRepository extends JpaRepository<ItemCatalogo, UUID
             WHERE i.ativo = true
               AND i.prestador.ativo = true
               AND (:busca = '' OR LOWER(i.titulo) LIKE LOWER(CONCAT('%', :busca, '%'))
-                               OR LOWER(i.descricao) LIKE LOWER(CONCAT('%', :busca, '%')))
+                               OR LOWER(i.descricao) LIKE LOWER(CONCAT('%', :busca, '%'))
+                               OR LOWER(i.categoria.nome) LIKE LOWER(CONCAT('%', :busca, '%')))
               AND (:cidade = '' OR LOWER(i.prestador.endereco.cidade) LIKE LOWER(CONCAT('%', :cidade, '%')))
             """
     )
